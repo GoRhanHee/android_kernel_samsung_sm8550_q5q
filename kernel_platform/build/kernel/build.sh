80a3f22f135e8553144a377a1f3221f4e6d42ef3
GoRhanHee <gorhanhee01@gmail.com>
2025-11-02 03:24:59 +0900

    defconfig : added custom configs in scripts

--- kernel_platform/build/kernel/build.sh
+++ kernel_platform/build/kernel/build.sh
@@ -708,6 +708,13 @@ if [ "${SKIP_DEFCONFIG}" != "1" ] ; then
     eval ${POST_DEFCONFIG_CMDS}
     set +x
   fi
+
+    #custom defconfig
+    echo "========================================================"
+    echo " Merging custom defconfig with .config"
+    (cd ${OUT_DIR} && ${MERGE_CONFIG} -m .config ${ANDROID_BUILD_TOP}/custom_defconfigs/custom_defconfig)
+    (cd ${OUT_DIR} && make O=${OUT_DIR} ${TOOL_ARGS} olddefconfig)
+    
 fi
 
 if [ "${KASAN}" = "sw_tags" ]; then
