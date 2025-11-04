30320f8e966e09f63a023b08f90ed3f53f4e02d8
GoRhanHee <gorhanhee01@gmail.com>
2025-11-05 01:00:11 +0900

    vendor_custom: edit path code

--- vendor_custom/vendor_boot.sh
+++ vendor_custom/vendor_boot.sh
@@ -4,15 +4,15 @@ export vendor_boot_pwd=$(pwd)
 
 # build vendor_boot modules
 ./LKM_Tools/02.prepare_vendor_boot_modules.sh \
-  ./LKM_Tools/vendor_boot/modules_list.txt \
-  ../out/msm-kalama-kalama-gki/dist \
-  ./LKM_Tools/vendor_boot/modules.load \
-  ../out/msm-kalama-kalama-gki/dist/System.map \
-  ../kernel_platform/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip \
-  ./LKM_Tools/vendor_boot/vendor_boot_modules || exit 1
+  $GITHUB_WORKSPACE/vendor_custom/LKM_Tools/vendor_boot/modules_list.txt \
+  $GITHUB_WORKSPACE/out/msm-kalama-kalama-gki/dist \
+  $GITHUB_WORKSPACE/vendor_custom/LKM_Tools/vendor_boot/modules.load \
+  $GITHUB_WORKSPACE/out/msm-kalama-kalama-gki/dist/System.map \
+  $GITHUB_WORKSPACE/kernel_platform/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip \
+  $GITHUB_WORKSPACE/vendor_custom/LKM_Tools/vendor_boot/vendor_boot_modules || exit 1
 
 # copy modules
-cp ./LKM_Tools/vendor_boot/vendor_boot_modules/* vendor_boot_img/build/unzip_boot/root.1/lib/modules/
+cp $GITHUB_WORKSPACE/vendor_custom/LKM_Tools/vendor_boot/vendor_boot_modules/* $GITHUB_WORKSPACE/vendor_custom/vendor_boot_img/build/unzip_boot/root.1/lib/modules/
 
 # build vendor_boot.img
 cd vendor_boot_img
@@ -20,4 +20,4 @@ cd vendor_boot_img
 
 # copy vendor_boot.img
 cd ${vendor_boot_pwd}
-cp ./vendor_boot_img/vendor_boot.img.signed ../gorhanhee/vendor_boot.img
+cp $GITHUB_WORKSPACE/vendor_custom/vendor_boot_img/vendor_boot.img.signed $GITHUB_WORKSPACE/gorhanhee/vendor_boot.img
--- vendor_custom/vendor_dlkm.sh
+++ vendor_custom/vendor_dlkm.sh
@@ -4,24 +4,24 @@ export vendor_dlkm_pwd=$(pwd)
 
 # build vendor_dlkm modules
 ./LKM_Tools/03.prepare_vendor_dlkm.sh \
-  ./LKM_Tools/vendor_dlkm/modules_list.txt \
-  ../out/msm-kalama-kalama-gki/dist \
-  ./LKM_Tools/vendor_dlkm/modules.load \
-  ../out/msm-kalama-kalama-gki/dist/System.map \
-  ../kernel_platform/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip \
-  ./LKM_Tools/vendor_dlkm/vendor_dlkm_modules \
+  $GITHUB_WORKSPACE/vendor_custom/LKM_Tools/vendor_dlkm/modules_list.txt \
+  $GITHUB_WORKSPACE/out/msm-kalama-kalama-gki/dist \
+  $GITHUB_WORKSPACE/vendor_custom/LKM_Tools/vendor_dlkm/modules.load \
+  $GITHUB_WORKSPACE/out/msm-kalama-kalama-gki/dist/System.map \
+  $GITHUB_WORKSPACE/kernel_platform/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip \
+  $GITHUB_WORKSPACE/vendor_custom/LKM_Tools/vendor_dlkm/vendor_dlkm_modules \
   "" \
   ""
 
 # copy modules
-cp ./LKM_Tools/vendor_dlkm/vendor_dlkm_modules/* vendor_dlkm/EXTRACTED_IMAGES/extracted_vendor_dlkm/lib/modules/
+cp $GITHUB_WORKSPACE/vendor_custom/LKM_Tools/vendor_dlkm/vendor_dlkm_modules/* $GITHUB_WORKSPACE/vendor_custom/vendor_dlkm/EXTRACTED_IMAGES/extracted_vendor_dlkm/lib/modules/
 
 # build vendor_dlkm.img
 cd vendor_dlkm
 sudo ./repack-erofs-script \
-  ./EXTRACTED_IMAGES/extracted_vendor_dlkm/ \
+  $GITHUB_WORKSPACE/vendor_dlkm/EXTRACTED_IMAGES/extracted_vendor_dlkm/ \
   erofs
 
 # copy vendor_dlkm.img
 cd ${vendor_dlkm_pwd}
-cp ./vendor_dlkm/REPACKED_IMAGES/vendor_dlkm_repacked.img ../gorhanhee/vendor_dlkm.img
+cp $GITHUB_WORKSPACE/vendor_custom/vendor_dlkm/REPACKED_IMAGES/vendor_dlkm_repacked.img $GITHUB_WORKSPACE/gorhanhee/vendor_dlkm.img
