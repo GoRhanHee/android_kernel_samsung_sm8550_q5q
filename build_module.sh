797dbf7d50c2a9d0d9a0860d449ad78ff9c05458
GoRhanHee <gorhanhee01@gmail.com>
2025-11-05 01:14:34 +0900

    build.sh: add fastbootD patched recovery & cooked boot.img code

--- build_module.sh
+++ build_module.sh
@@ -20,11 +20,15 @@ cd ${ANDROID_BUILD_TOP}
 mkdir gorhanhee
 export GORHANHEE=${ANDROID_BUILD_TOP}/gorhanhee
 
-#4. build vendor_boot & vendor_dlkm
+# 4. build vendor_boot & vendor_dlkm
 cd vendor_custom
 ./vendor_boot.sh || exit 1
 ./vendor_dlkm.sh || exit 1
 
-#5. Make Odin flashable .tar file
+# 5. Prepare make odin flashable .tar file
+cp "${ANDROID_BUILD_TOP}/out/msm-${CHIPSET_NAME}-${CHIPSET_NAME}-${TARGET_PRODUCT}/dist/boot.img" "${GORHANHEE}/"
+cp "${ANDROID_BUILD_TOP}/vendor_custom/recovery.img" "${GORHANHEE}/"
+
+# 6. Make Odin flashable .tar file
 cd ${GORHANHEE} 
-tar -cvf Galaxy_Fold5_SuSFS.tar boot.img vendor_boot.img
+tar -cvf Galaxy_Fold5_SuSFS.tar boot.img recovery.img vendor_boot.img
