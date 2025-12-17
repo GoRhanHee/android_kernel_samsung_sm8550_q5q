4149b863f13910ebee7549f63297b52c6fb718f2
GoRhanHee <gorhanhee01@gmail.com>
2025-12-17 23:04:09 +0900

    build: edit os_patch_level date

--- build.sh
+++ build.sh
@@ -115,7 +115,7 @@ export GKI_KERNEL_BUILD_OPTIONS="
 # MKBOOTIMG Setting
 export MKBOOTIMG_EXTRA_ARGS="
     --os_version 13.0.0 \
-    --os_patch_level 2025-08-00 \
+    --os_patch_level 2025-11-00 \
     --pagesize 4096 \
 "
 
