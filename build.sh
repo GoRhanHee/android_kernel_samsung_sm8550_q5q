1f43b4754b148b0e673869deced83c09aab76cec
nullptr-t-oss <127822226+nullptr-t-oss@users.noreply.github.com>
2026-03-29 10:54:00 +0000

    add: missing QCOM EXT modules
    Signed-off-by: nullptr-t-oss <127822226+nullptr-t-oss@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -46,6 +46,24 @@ export KBUILD_EXT_MODULES="../vendor/qcom/opensource/mm-drivers/msm_ext_display 
   ../vendor/qcom/opensource/display-drivers/msm \
   ../vendor/qcom/opensource/audio-kernel \
   ../vendor/qcom/opensource/camera-kernel \
+  ../vendor/qcom/opensource/video-driver \
+  ../vendor/qcom/opensource/graphics-kernel \
+  ../vendor/qcom/opensource/dataipa/drivers/platform/msm \
+  ../vendor/qcom/opensource/datarmnet/core \
+  ../vendor/qcom/opensource/datarmnet-ext/aps \
+  ../vendor/qcom/opensource/datarmnet-ext/offload \
+  ../vendor/qcom/opensource/datarmnet-ext/shs \
+  ../vendor/qcom/opensource/datarmnet-ext/sch \
+  ../vendor/qcom/opensource/datarmnet-ext/perf \
+  ../vendor/qcom/opensource/datarmnet-ext/perf_tether \
+  ../vendor/qcom/opensource/datarmnet-ext/wlan \
+  ../vendor/qcom/opensource/eva-kernel \
+  ../vendor/qcom/opensource/wlan/platform \
+  ../vendor/qcom/opensource/bt-kernel \
+  ../vendor/nxp/opensource/driver \
+  ../vendor/st/opensource/driver \
+  ../vendor/st/opensource/eSE-driver \
+  ../vendor/qcom/opensource/wlan/qcacld-3.0 \
   "
 
 # Build Setting
