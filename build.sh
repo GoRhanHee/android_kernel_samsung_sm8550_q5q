5db88370b1012a80ada87febf06166a2090724dc
GoRhanHee <gorhanhee01@gmail.com>
2026-03-31 00:28:03 +0900

    prebuilts: import cook vendor_boot & vendor_dlkm system

--- build.sh
+++ build.sh
@@ -118,3 +118,14 @@ tar -xf "$TOOLCHAIN_FILE" -C kernel_platform --strip-components=1 toolchain/preb
 
 # Cooking Kernel Soruce & boot.img
 ( env ${GKI_KERNEL_BUILD_OPTIONS} ${ANDROID_BUILD_TOP}/kernel_platform/build/android/prepare_vendor.sh sec ${TARGET_PRODUCT} || exit 1)
+
+# Copying boot.img
+cp ./out/msm-${CHIPSET_NAME}-${CHIPSET_NAME}-${TARGET_PRODUCT}/dist/boot.img ./boot.img
+
+# Cooking vendor_boot.img
+    SCRIPT_DIR="${SCRIPT_DIR}" \
+        "${SCRIPT_DIR}/prebuilts/build_vendor_boot.sh" || exit 1
+
+# Cooking vendor_dlkm.img
+    SCRIPT_DIR="${SCRIPT_DIR}" \
+        "${SCRIPT_DIR}/prebuilts/build_vendor_dlkm.sh" || exit 1
