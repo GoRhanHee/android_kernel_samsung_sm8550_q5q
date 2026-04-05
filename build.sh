0794a1bc1dff8f0c0525b788f3605bc8334c055b
GoRhanHee <gorhanhee01@gmail.com>
2026-04-06 03:05:50 +0900

    build: add Patch SuSFS code

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 # Import common kernel
 git submodule update --init --remote --depth 1 kernel_platform/common
 
-# Import KernelSU-Next with SuSFS
-(cd kernel_platform/common && curl -LSs "https://raw.githubusercontent.com/pershoot/KernelSU-Next/dev-susfs/kernel/setup.sh" | bash -s dev-susfs)
+# Import KernelSU-Next
+(cd kernel_platform/common && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -)
+
+# Patch SuSFS
+patch -p1 -d kernel_platform/common < prebuilts/ksun.patch
 
 # DIR Setting
 SCRIPT_DIR="$(dirname $(readlink -fq $0))"
